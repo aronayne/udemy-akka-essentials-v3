bd2510517580a534ab0d01a68631076a9ca069a2
aronayne <adrian.ronayne@gmail.com>
2020-07-30 21:16:11 +0100

    update

---
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -0,0 +1,52 @@
+package part2actors
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+object ActorsIntro extends App{
+
+  // part1 - actor systems
+  val actorSystem = ActorSystem("firstActorSystem")
+  println(actorSystem.name)
+
+  // part2 - create actors
+  // word count actor
+
+  class WordCountActor extends Actor {
+
+    var totalWords = 0
+
+    override def receive: PartialFunction[Any, Unit] = {
+      case message: String =>
+        println(s"[word counter] I have received: $message")
+        totalWords += message.split(" ").length
+      case msg => println(s"[word counter] I cannot understand ${msg.toString}")
+    }
+  }
+
+  // part3 - instantiate our actor
+  val wordCounter = actorSystem.actorOf(Props[WordCountActor], "wordCounter")
+  val anotherWordCounter = actorSystem.actorOf(Props[WordCountActor], "anotherWordCounter")
+
+  // part4 - communicate!
+  wordCounter ! "I am learning Akka and it's pretty damn cool!"
+  anotherWordCounter ! "A different message"
+  // asynchronous - messages are sent asynchronously
+
+//  new WordCountActor //You cannot create an instance of [part2actors.ActorsIntro$WordCountActor] explicitly using the constructor (new)
+
+  object Person {
+    def props(name: String) = Props(new Person(name))
+  }
+
+  class Person(name: String) extends Actor {
+    override def receive: Receive = {
+      case "hi" => println(s"Hi, my name is $name")
+      case _ =>
+    }
+  }
+
+//  val person = actorSystem.actorOf(Props(new Person("Bob")))
+  val person = actorSystem.actorOf(Person.props("Bob"))
+  person ! "hi"
+  
+}
