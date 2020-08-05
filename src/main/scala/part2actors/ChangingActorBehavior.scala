e649ffeff7fe321341a1de37e5a0c3b76eef0be6
aronayne <adrian.ronayne@gmail.com>
2020-08-05 19:12:55 +0100

    update

--- src/main/scala/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -1,7 +1,175 @@
 package part2actors
 
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import part2actors.ActorCapabilities.{Counter, system}
+import part2actors.ChangingActorBehavior.Mom.MomStart
+
 object ChangingActorBehavior extends App {
 
+  object FussyKid {
+    case object KidAccept
+    case object KidReject
+    val HAPPY = "happy"
+    val SAD = "sad"
+  }
 
+  class FussyKid extends Actor {
+    import FussyKid._
+    import Mom._
+    // internal state of the kid
+    var state = HAPPY
+    override def receive: Receive = {
+      case Food(VEGETABLE) => state = SAD
+      case Food(CHOCOLATE) => state = HAPPY
+      case Ask(_) =>
+        if (state == HAPPY) sender() ! KidAccept
+        else sender() ! KidReject
+    }
+  }
+
+  class StatelessFussyKid extends Actor {
+
+    import FussyKid._
+    import Mom._
+
+    override def receive: Receive = happyReceive
+
+    def happyReceive: Receive = {
+      case Food(VEGETABLE) => context.become(sadReceive, false) // change my receive handler to sadReceive
+      case Food(CHOCOLATE) =>
+      case Ask(_) => sender() ! KidAccept
+    }
+
+    def sadReceive: Receive = {
+      case Food(VEGETABLE) => context.become(sadReceive , false)
+      case Food(CHOCOLATE) => context.unbecome() //change my receive handler to happyReceive
+      case Ask(_) => sender() ! KidReject
+    }
+  }
+
+  object Mom {
+    case class MomStart(kidRef: ActorRef)
+    case class Food(food: String)
+    case class Ask(message: String) //do you want to play ?
+    val VEGETABLE = "veggies"
+    val CHOCOLATE = "chocolate"
+  }
+  class Mom extends Actor {
+    import Mom._
+    import FussyKid._
+    override def receive: Receive = {
+      case MomStart(kidRef) =>
+        // test our interaction
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Food(CHOCOLATE)
+        kidRef ! Food(CHOCOLATE)
+        kidRef ! Ask("do you want to play?")
+      case KidAccept => println("Yay, my kid is happy!")
+      case KidReject => println("My kid is sad, but as he's healthy!")
+    }
+  }
+
+  val system = ActorSystem("changingActorBehaviorDemo")
+  val fussyKid = system.actorOf(Props[FussyKid])
+  val statelessFussyKid = system.actorOf(Props[StatelessFussyKid])
+  val mom = system.actorOf(Props[Mom])
+
+//  mom ! (MomStart(fussyKid))
+
+  mom ! MomStart(statelessFussyKid)
+  /*
+   mom receives MomStart
+     kid receives Food(veg) -> kid will change the handler to sadReceive
+     kid receives Ask(play?) -> kid replies with the sadReceive handler =>
+   mom receives KidReject
+   */
+
+  /*
+
+  context.become
+
+  Food(veg) -> stack.push(sadReceive)
+  Food(chocolate) -> stack.push(happyReceive)
+
+  Stack:
+  1. happyReceive
+  2. sadReceive
+  3. happyReceive
+
+   */
+
+  /*
+  new behavior
+  Food(veg)
+  Food(veg)
+  Food(chocolate)
+  Food(chocolate)
+
+  State:
+  1. happyReceive
+   */
+
+  /**
+   * Exercises
+   * 1 - recreate the Counter Actor with context.become and NO MUTABLE state
+   */
+
+  object Counter {
+    case object Increment
+    case object Decrement
+    case object Print
+  }
+
+  class Counter extends Actor {
+    import Counter._
+
+    override def receive: Receive = {
+     ???
+    }
+  }
+
+  import Counter._
+  val counter = system.actorOf(Props[Counter] , "myCounter")
+
+  (1 to 5).foreach(_ => counter ! Increment)
+  (1 to 3).foreach(_ => counter ! Decrement)
+  counter ! Print
+
+  /**
+   * Exercise 2 - a simplified voting system
+   */
+  case class Vote(candidate: String)
+  class Citizen extends Actor {
+    override def receive: Receive = ??? //TODO
+  }
+
+  case class AggregateVotes(citizens: Set[ActorRef])
+  case object VoteStatusRequest
+  case class VoteStatusReply(candidate: Option[String])
+  class VoteAggregator extends Actor {
+    override def receive: Receive = ???
+  }
+
+  val alice = system.actorOf(Props[Citizen])
+  val bob = system.actorOf(Props[Citizen])
+  val charlie = system.actorOf(Props[Citizen])
+  val daniel = system.actorOf(Props[Citizen])
+
+  alice ! Vote("Martin")
+  bob ! Vote("Jonas")
+  charlie ! Vote("Roland")
+  daniel ! Vote("Roland")
+
+  val voteAggregator = system.actorOf(Props[VoteAggregator])
+  voteAggregator ! AggregateVotes(Set(alice, bob, charlie, daniel))
+
+  /*
+  Print the status of the votes
+
+  Martin -> 1
+  Jonas -> 1
+  Roland -> 2
+   */
 
 }
